58bbd581cd31f2bcde101c0b90d6a340d12284a4
Tien Dung <dungtn@gmail.com>
2023-02-20 06:37:22 +0700

    cleanup

--- gpt-neox/preprocess_data.sh
+++
@@ -1,7 +0,0 @@
-python3 preprocess_data.py \
-            --input ../data/sample.jsonl \
-            --output-prefix ../data/mydataset \
-            --vocab ../qna/qna2.vocab \
-            --dataset-impl mmap \
-            --tokenizer-type SPMTokenizer \
-            --append-eod
