2c3c9c42ccdb5872afc3359a66653e11f544e541
Tien Dung <dungtn@gmail.com>
2023-02-26 20:08:54 +0700

    update

---
+++ stats.py
@@ -0,0 +1,40 @@
+def scan_docs(filename, maxx=None):
+    texts = open(filename).read().split("\n")[:-1]
+    max_len = 0;  min_len = 99999999;  total = 0
+    max_para = 0; min_para = 99999999; total_para = 0
+    max_tk = 0;   min_tk = 99999999;   total_tk = 0
+
+    if maxx is None: maxx = len(texts)
+    for i in range(maxx):
+        text = texts[i]
+        n = len(text)
+        # if n < 100: continue
+        if n > max_len: max_len = n
+        if n < min_len: min_len = n
+        total += n
+
+        para_n = text.count('\\n') + 1
+        if para_n > max_para: max_para = para_n
+        if para_n < min_para: min_para = para_n
+        total_para += para_n
+
+        tk_n = text.count(' ') + para_n
+        if tk_n > max_tk: max_tk = tk_n
+        if tk_n < min_tk: min_tk = tk_n
+        total_tk += tk_n
+    print(f"""\n>>>Found {maxx} documents in {filename}:
+Characters: max {max_len}, min {min_len}, avg {total // maxx}.
+Est Tokens: max {max_tk}, min {min_tk}, avg {total_tk // maxx}.
+Paragraphs: max {max_para}, min {min_para}, avg {total_para // maxx}.
+""")
+
+prefixs = [
+    # "vi_meta_part_1",
+    # "vi_meta_part_99",
+    # "vi_meta_part_26",
+    # "vi_meta_part_35",
+    "all"
+]
+
+for prefix in prefixs:
+    scan_docs(f"{prefix}.txt")
